34486843217217a8266632826232f561358c0cbf
sathvikreddy369 <satreddyyy@gmail.com>
2025-08-03 16:56:29 +0530

    first

--- index.js
+++ index.js
@@ -23,7 +23,26 @@ app.get("/", function (req, res) {
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
-
+app.get("/api/:date?",(req,res)=>{
+  const dateParam =req.params.date;
+  let date;
+  if(!dateParam){
+    date=new Date();
+  }else{
+    if(!isNaN(dateParam) && /^\d+$/.test(dateParam)){
+      date=new Date(parseInt(dateParam));
+    }else{
+      date=new Date(dateParam);
+    }
+  }
+  if(date.toString()==='Invalid Date'){
+    return res.json({error:"Invalid Date"});
+  }
+  res.json({
+    unix:date.getTime(),
+    utc:date.toUTCString()
+  })
+});
 
 
 // Listen on port set in environment variable or default to 3000
